1669eb5f93ddc9eb67498b78449aa79f7013c57c
Colin Marshall <colin.marshall94@gmail.com>
2023-01-04 20:40:18 -0800

    debugging node modules

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const { databaseVersion } = require('../../../gitlab/UW-VIRT-FSF-PT-09-2022-U-LOLC/13-ORM/01-Activities/26-Stu_Literals/Solved/config/connection.js');
 
 const sequelize = require('../config/connection.js');
 
@@ -7,6 +8,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,12 +4,20 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// products to categories is one-to-many.  product has one category, categories have many products
+
 // Products belongsTo Category
+Product.belongsTo(Category);
 
-// Categories have many Products
+// Category has many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
+// TODO: one-to-many relationship
 // Products belongToMany Tags (through ProductTag)
 
+// one-to-many relationships
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
